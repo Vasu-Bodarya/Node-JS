4197fc5c80fbeddee19237f4f64c2a40d1dec658
vasubodarya <vasubodariya7707@gmaiol.com>
2025-04-02 13:14:02 +0530

    02/04/25

---
+++ Practical Exam/Practical Exam/models/crudModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
